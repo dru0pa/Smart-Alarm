811de3c0c307e5490aee381ba990c1a608564583
dru0pa <dru0pa@gmail.com>
2016-09-22 07:14:22 +0000

    Delete CalendarCredentials.py

--- CalendarCredentials.py
+++
@@ -1,8 +0,0 @@
-CLIENT_ID='38506302342-uvj3sp28l73v39dbsu1dtckkoojkg3o9.apps.googleusercontent.com'
-CLIENT_SECRET='wIM0b-gC402WH1IShrvujyb1'
-DEVELOPER_KEY='AIzaSyDO2QdifjLfb2SQFvSVuXfSO39T2yL1Y-w'
-CALENDAR='https://www.googleapis.com/auth/calendar'
-PROJECT_ID='alarmpi-1371'
-AUTH_URI= 'https://accounts.google.com/o/oauth2/auth'
-OWM_APPID='2eb1c96ee1f6057e97bb766c8a9980ae'
-
